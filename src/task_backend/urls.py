b9f6cbc21b1fb38de2f4f24947c58168bd75132c
Ryan Omoruyi <ryan.omoruyi@ibm.com>
2019-06-09 15:47:19 -0400

    update name in config files

--- src/task_backend/urls.py
+++ src/task_backend/urls.py
@@ -1,4 +1,4 @@
-"""todo_backend URL Configuration
+"""task_backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
