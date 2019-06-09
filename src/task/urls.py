ba00527973db72238aa4ff8d6370ab4ae1aaef5a
Ryan Omoruyi <ryan.omoruyi@ibm.com>
2019-06-09 17:59:33 -0400

    change todos to tasks

--- src/task/urls.py
+++ src/task/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 # Create a router and register our viewsets with it
 router = DefaultRouter(trailing_slash=False)
-router.register(r'todos', views.TaskItemViewSet)
+router.register(r'tasks', views.TaskItemViewSet)
 
 # The API URLs are now determined automatically by the router
 urlpatterns = [
